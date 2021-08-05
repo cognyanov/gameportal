397ededca659cb37fefbeae5f5f083a5159ba11a
cognyanov <79589328+cognyanov@users.noreply.github.com>
2021-08-05 21:33:11 +0300

    Add files via upload

---
+++ src/app/games-info/games-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-games-info',
+  templateUrl: './games-info.component.html',
+  styleUrls: ['./games-info.component.css']
+})
+export class GamesInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
