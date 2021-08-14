caa752fa070021f2191ae21251c7c56989b9019d
cognyanov <79589328+cognyanov@users.noreply.github.com>
2021-08-14 23:30:30 +0300

    Add files via upload

---
+++ src/app/guess-the-card/guess-the-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GuessTheCardComponent } from './guess-the-card.component';
+
+describe('GuessTheCardComponent', () => {
+  let component: GuessTheCardComponent;
+  let fixture: ComponentFixture<GuessTheCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GuessTheCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GuessTheCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
